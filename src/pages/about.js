00f346e8042c976f4d1edd414e2085a72db902e0
Thomas Strub <thomstrub@gmail.com>
2021-01-30 22:27:21 -0800

    adds part two of Gatsby tutorial

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from "react"
+import Header from '../components/header'
+import {Link} from "gatsby"
 
-
-export default function Home() {
+export default function About() {
   return(
-    <>
-    
-    <div >About</div>
-    </>
+    <div style={{ color: `teal` }}>
+        <Link to="/">Home</Link>
+      <Header headerText="About Gatsby"/>
+      <Header headerText="How is this different than react?"/>
+      <p>Such wow. Very React.</p>
+    </div>
   ) 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default function Home() {
   return (
     <>
+    <Link to="/contact/">Contact</Link>
+    <Link to="/about/">About</Link>
     <div style={{ color: `purple`, fontSize: `72px` }}>Doctor Serenity</div>
     <div style={{color: 'purple'}}> <p>What a world</p></div>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
     </>
   ) 
 }
