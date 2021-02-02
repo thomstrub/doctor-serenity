7cdc587eb2bd8fb23b9bbb570fe76aa3b0296f55
Thomas Strub <thomstrub@gmail.com>
2021-02-01 16:53:21 -0800

    adds navigation and typography plugin to tutorial site

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
 import React from "react"
 import Header from '../components/header'
 import {Link} from "gatsby"
+import Layout from "../components/layout"
 
 export default function About() {
   return(
-    <div style={{ color: `teal` }}>
-        <Link to="/">Home</Link>
-      <Header headerText="About Gatsby"/>
-      <Header headerText="How is this different than react?"/>
-      <p>Such wow. Very React.</p>
-    </div>
+      <Layout>
+        <div style={{ color: `teal` }}>
+            <Link to="/">Home</Link>
+        <Header headerText="About Gatsby"/>
+        <Header headerText="How is this different than react?"/>
+        <p>Such wow. Very React.</p>
+        </div>
+      </Layout>
+    
   ) 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import React from "react"
 import {Link} from "gatsby"
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <>
-    <Link to="/contact/">Contact</Link>
-    <Link to="/about/">About</Link>
-    <div style={{ color: `purple`, fontSize: `72px` }}>Doctor Serenity</div>
-    <div style={{color: 'purple'}}> <p>What a world</p></div>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <Layout>
+      <div style={{ color: `purple`, fontSize: `72px` }}>Doctor Serenity</div>
+      <div style={{color: 'purple'}}> <p>What a world</p></div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <div>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>
+          What do I like to do? Lots of course but definitely enjoy building
+          websites.
+        </p>
+      </div>
+    </Layout>
+    
     </>
   ) 
 }
