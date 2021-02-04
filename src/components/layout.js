bd8895f8abd190f0b570d7e2c9bff1b48670cc27
Thomas Strub <thomstrub@gmail.com>
2021-02-03 17:50:20 -0800

    adds basic nav and logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Link } from "gatsby"
+import Nav from './nav/nav'
 
-const ListLink = props => (
-    <li style={{display: 'inline-block', marginRight: '1rem'}}>
-        <Link to={props.to}>{props.children}</Link>
-    </li>
-)
 
 export default function Layout({ children }) {
     return (
-        <div style={{ margin: '3rem auto', maxWidth: 650, padding: '0 1rem' }}>
-            <header style={{ marginBottom: '1.5rem' }}>
-                <Link to="/" style={{ textShadow: 'none', backgroundImage: 'none '}}>
-                    <h3 style={{display: 'inline'}}>Cool Cool Website</h3>
-                </Link>
-                <ul style={{ listStyle: 'none', float: 'right' }}>
-                    <ListLink to="/">Home</ListLink>
-                    <ListLink to="/about">About</ListLink>
-                    <ListLink to="/contact">Contact</ListLink>
-                </ul>
+        <div>
+            <header>
+                <Nav />
             </header>
             {children}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import React from "react"
 import {Link} from "gatsby"
+import {Helmet} from "react-helmet"
 import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <>
+    <Helmet>
+          <meta charSet="utf-8" />
+          <title>Doctor Serenity</title>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossorigin="anonymous"
+          />
+        </Helmet>
     <Layout>
       <div style={{ color: `purple`, fontSize: `72px` }}>Doctor Serenity</div>
       <div style={{color: 'purple'}}> <p>What a world</p></div>
