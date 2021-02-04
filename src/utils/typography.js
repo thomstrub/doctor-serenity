0261c57f2169a968ae17e6385e8b76b6d4cbd60c
Thomas Strub <thomstrub@gmail.com>
2021-02-03 22:16:18 -0800

    removes tutorial theme to eliminate hover state

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,7 +2,13 @@ import Typography from "typography"
 import fairyGateTheme from "typography-theme-fairy-gates"
 
 
-const typography = new Typography(fairyGateTheme)
+const typography = new Typography({
+    baseFontSize: '18px',
+    baseLineHeight: 1.45,
+    headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+    bodyFontFamily: ['Merriweather', 'serif'],
+    // See below for the full list of options.
+  })
 
 export const { scale, rhythm, options } = typography
 export default typography
