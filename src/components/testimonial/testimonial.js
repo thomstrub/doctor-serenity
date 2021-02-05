85422961a4bf984372902ba66cc5f80401b9fe93
Thomas Strub <thomstrub@gmail.com>
2021-02-04 20:47:49 -0800

    adds css file and basic testimonial component

--- src/components/testimonial/testimonial.js
+++ src/components/testimonial/testimonial.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import './testimonial.css'
+
+export default function Testimonial({blurb, name, image}){
+    return(
+        <div className="col-md-4">
+            <p className="testimonial-blurb">
+            {blurb}
+            </p>
+            <img src={image} alt="Tara" className="testimonial-photo"/>
+            <p className="testimonial-name">{name}</p>
+            <p className="small caption">This testimonial was submitted by a paid participant</p>
+        </div>
+    )
+}
