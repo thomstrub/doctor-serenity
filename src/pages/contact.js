6f7071e36d43dd3c5e2bd4bf477e943d6e684f07
Thomas Strub <thomstrub@gmail.com>
2021-02-04 22:09:06 -0800

    adds basic contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,57 @@
 import React from "react"
+import './index.css'
 import { Link } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
+import {Helmet} from "react-helmet"
 
 export default function Contact() {
   return (
+    <>
+    <Helmet>
+          <meta charSet="utf-8" />
+          <title>Doctor Serenity</title>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossorigin="anonymous"
+          />
+    </Helmet>
     <Layout>
-        <div style={{ color: `teal` }}>
-        <Link to="/">Home</Link>
-        <Header headerText="Contact" />
-        <h1>I'd love to talk! Email me at the address below</h1>
-        <p>
-            <a href="mailto:me@example.com">me@example.com</a>
-        </p>
-        </div>
-
+    <div className="container-md">
+    <h1 className="display-3">Get in touch to begin your journey.</h1>
+    </div>
+    <div className="container">
+        <form className="email">
+            <div className="form-group">
+                <label for="exampleFormControlInput1">Email address</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div className="form-group">
+                <label for="exampleFormControlSelect1">Subject</label>
+                <select className="form-control" id="exampleFormControlSelect1">
+                  <option>New Client Application</option>
+                  <option>Services Inquiry</option>
+                  <option>Complaint: My appointment was canceled</option>
+                  <option>Complaint: I am having trouble taking your advice</option>
+                  <option>Complaint: I've hired a lawyer</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <label for="exampleFormControlTextarea1">Example textarea</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+            </div>
+            <button type="button" className="btn btn-primary btn-lg">Submit</button>
+        </form>
+    </div>
+    <footer>
+    <p>
+        Cool website by Thom Strub
+    </p>
+    </footer>
     </Layout>
-
+    </>
     
   )
 }
