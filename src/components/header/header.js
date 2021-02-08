9b66c33d50e566cc1c13c767c5d5b84c7deebe9d
Thomas Strub <thomstrub@gmail.com>
2021-02-07 20:20:46 -0800

    experiment with nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,34 @@
 import React from "react"
 import './header.css'
 import { Link } from "gatsby"
-import {Nav} from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 
 const ListLink = props => (
-    <li className= "nav-item">
+    <Nav.Item>
+        <li className= "nav-item">
         <Link className="nav-link active" to={props.to}>{props.children}</Link>
-    </li>
+        </li>
+    </Nav.Item>
+    
 )
 
 
 export default function Header() {
   return(
       <>
-        <div className="container">
+        <Navbar >
+        <Navbar.Brand href="#home">
+            <img
+                alt=""
+                src="_Images/logo.jpg"
+                width="200"
+                
+                className="d-inline-block align-top"
+            />{' '}
+            React Bootstrap
+            </Navbar.Brand>
+        </Navbar>
+        <Nav className="justify-content-end">
             <div className="logo">
                 <img style={{maxWidth: "20vw", display: "inline"}} src="_Images/logo.jpg"  alt="Dr. Serenity Life Coaching" />
             </div>
@@ -23,8 +38,23 @@ export default function Header() {
             <ListLink to="/services">Services</ListLink>
             <ListLink to="/contact">Contact</ListLink>
         </ul>
-        </div>
-
+        </Nav>
+        <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+            <ListLink to="/">Home</ListLink>
+            </Nav.Item>
+            <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+                Disabled
+            </Nav.Link>
+        </Nav.Item>
+  </Nav>
       </>
   )
     
