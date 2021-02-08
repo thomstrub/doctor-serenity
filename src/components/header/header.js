843267bd1aa0f137dc0330601a822708ca2932a8
Thomas Strub <thomstrub@gmail.com>
2021-02-07 20:43:23 -0800

    simplifying bootstrap nav component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,10 +4,9 @@ import { Link } from "gatsby"
 import { Nav, Navbar } from 'react-bootstrap'
 
 const ListLink = props => (
-    <Nav.Item>
-        <li className= "nav-item">
-        <Link className="nav-link active" to={props.to}>{props.children}</Link>
-        </li>
+    <Nav.Item as={Link} className="nav-link active" to={props.to}>
+        {props.children}
+        
     </Nav.Item>
     
 )
@@ -16,45 +15,26 @@ const ListLink = props => (
 export default function Header() {
   return(
       <>
-        <Navbar >
-        <Navbar.Brand href="#home">
+        <Navbar style={{display: "flex", justifyContent: "space-between"}}>
+        <Navbar.Brand as={Link} to="/">
             <img
                 alt=""
                 src="_Images/logo.jpg"
-                width="200"
+                width="300"
                 
-                className="d-inline-block align-top"
+                className="d-inline-block"
             />{' '}
-            React Bootstrap
             </Navbar.Brand>
-        </Navbar>
-        <Nav className="justify-content-end">
-            <div className="logo">
-                <img style={{maxWidth: "20vw", display: "inline"}} src="_Images/logo.jpg"  alt="Dr. Serenity Life Coaching" />
-            </div>
-        <ul className= "nav justify-content-center">
-            <ListLink to="/">Home</ListLink>
-            <ListLink to="/about">About</ListLink>
-            <ListLink to="/services">Services</ListLink>
-            <ListLink to="/contact">Contact</ListLink>
-        </ul>
+            <Nav className="justify-content-end">
+            
+                <ListLink to="/">Home</ListLink>
+                <ListLink to="/about">About</ListLink>
+                <ListLink to="/services">Services</ListLink>
+                <ListLink to="/contact">Contact</ListLink>
+            
         </Nav>
-        <Nav className="justify-content-end" activeKey="/home">
-            <Nav.Item>
-            <ListLink to="/">Home</ListLink>
-            </Nav.Item>
-            <Nav.Item>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-            <Nav.Link eventKey="disabled" disabled>
-                Disabled
-            </Nav.Link>
-        </Nav.Item>
-  </Nav>
+        </Navbar>
+        
       </>
   )
     
