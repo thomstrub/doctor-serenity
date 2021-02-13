9b79b7d3fa38dc5186f3b1b3ae0ee1e2e2f74873
Thomas Strub <thomstrub@gmail.com>
2021-02-12 19:02:44 -0800

    removes container from layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,11 +4,11 @@ import Header from './header/header'
 
 export default function Layout({ children, page }) {
     return (
-        <div className="container-flex">
+        <>
             <header>
                 <Header page={page}/>
             </header>
             {children}
-        </div>
+        </>
     )
 }
--- src/components/randomApp/randomApp.js
+++ src/components/randomApp/randomApp.js
@@ -5,11 +5,11 @@ export default function RandomApp(){
     return(
         <div className="container">
             <div className="row">
-                <div className="col-md-3 offset-md-3">
+                <div className="col-md-3 offset-1">
                     <img src="_Images/randomalarm-app.jpg" className="img-fluid" alt="Random Alarm"/>
                 </div>
 
-                <div className="col-md-5">
+                <div className="col-md-6 offset-1">
                     <h3>Harness the power of random</h3>
                     <p>
                     I have innovated three proprietary methods that coordinate with each measured success. Together, they are called RandomAlarm™.
