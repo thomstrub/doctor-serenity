30c66603433bd4ea613d3bf1a4558ce31d42eb2e
Thomas Strub <thomstrub@gmail.com>
2021-02-26 12:12:06 -0800

    updates content for randomalarm page icons

--- src/pages/randomAlarm.js
+++ src/pages/randomAlarm.js
@@ -32,11 +32,26 @@ export default function Contact() {
         <div className="container">
           <div className="row">
             <div className="container">
-              <h2 className="h2heading">Coaching Techniques</h2>
+              <h2 className="h2heading">RandomAlarm™</h2>
             </div>
-            <Icon content="RandomNap™" page={state.page} icon="_Images/icon-sleep.svg"/>
-            <Icon content="RandomTask™" page={state.page} icon="_Images/icon-work.svg"/>
-            <Icon content="RandomNourish™" page={state.page} icon="_Images/icon-eat.svg"/>
+            <Icon 
+              title="RandomNap™"
+              content="RandomAlarm™ gently notifies you at random times throughout the day when you should take a nap or wake from restfulness. Frequent naps lead to frequent productivity and well-being." 
+              page={state.page} 
+              icon="_Images/icon-sleep.svg"
+              />
+            <Icon 
+            title="RandomTask™"
+            content="Starting and stopping work at random times throughout the day may seem counterintuitive, but Dr. Serenity’s informal research has proven otherwise. RandomAlarm™ will help." 
+            page={state.page} 
+            icon="_Images/icon-work.svg"
+            />
+            <Icon 
+            title="RandomNourish™"
+            content="RandomAlarm™ tells you when to eat based on random science. Not eating on a set schedule keeps your blood sugar at random levels throughout the day." 
+            page={state.page} 
+            icon="_Images/icon-eat.svg"
+            />
           </div>
         </div>
         {/* container */}
