b3c7f45ae75da5eb8c916dbb9c580ef4ed1e13f2
Thomas Strub <thomstrub@gmail.com>
2021-03-17 17:38:05 -0700

    adds css for hero

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -8,7 +8,7 @@ export default function Hero({image, blurb}){
           <div className="row">
           <div className="col" style={{minHeight: "300px", display: "flex", }}>
           <div className="container-fluid image">
-            <div className="col-lg-6">
+            <div className="col-lg-12">
               <h2 className="hero">{blurb}</h2>
             </div>
             
