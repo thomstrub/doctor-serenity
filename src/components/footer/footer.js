84ed4a6ede6ba8660ba13cb11c0ef1ea65efce16
Thomas Strub <thomstrub@gmail.com>
2021-03-23 23:24:04 -0700

    adds footer text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,8 @@ import './footer.css';
 
 export default function Footer(){
     return(
-        <footer id="footer"></footer>
+        <footer id="footer">
+            <div id="footer-text">© 2020 Dr. Serenity™. All Rights reserved.</div>
+        </footer>
     )
 }
