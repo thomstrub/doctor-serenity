3be782880c88b2942284b79e68c62aa26bee78aa
Thomas Strub <thomstrub@gmail.com>
2021-03-26 23:28:11 -0700

    addds button component to fundamentals component

--- src/components/fundamentals/fundamentals.js
+++ src/components/fundamentals/fundamentals.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./fundamentals.css"
+import Button from '../button/button'
 
 export default function Fundamentals(){
     return(
@@ -11,7 +12,7 @@ export default function Fundamentals(){
                     <p className="fundamentals">
                         My research and past experience have informed my proprietary techniques. I have found that “sleep”, “work”, and “diet” are the key ingredients to measured success. I have innovated three proprietary methods that coordinate with each measured success. Together, they are called RandomAlarm™.
                     </p>
-                    <a className="btn btn-primary session-button" href="#" role="button">Book a session now</a>
+                    <Button text="Book a session now"></Button>
                 </div>
                 <div className="col-md-4">
                     <img src="_Images/random-ven.png" alt="random-ven" className="img-fluid"/>
