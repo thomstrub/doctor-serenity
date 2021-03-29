dc2a3d1a3c0ac2e601442937212994f5f6fe6ef7
Thomas Strub <thomstrub@gmail.com>
2021-03-28 22:15:48 -0700

    adjusts hero text-box

--- src/components/homeHero/homeHero.js
+++ src/components/homeHero/homeHero.js
@@ -8,17 +8,24 @@ export default function HomeHero(){
         
         <div className="container-fluid home-hero-image" style={{backgroundImage: "url('_Images/hero.png')"}} >
             <div className="row">
-              <div className="hero-row">
-
-            <div className="col-md-6 offset-1" style={{minHeight: "300px", display: "flex", }}>
-          
-            <div className="container-fluid">
+              
+            <div className="col-md-6 offset-5" style={{minHeight: "300px", display: "flex", }}>
+            <div className="row hero-row">
+            <div className="col container-fluid home-hero-blurb">
               <h2 className="hero" style={{color: "black"}}>Do you need help getting your life <strong>together</strong>? Look no further.</h2>
               <p className="hero-sub">Let Dr. Serenity™ lead you through a life-changing journey with guaranteed* results.</p>
-              <Button text="Book a session now"/>
+              
               <p className="footnote">*not legally binding</p>
             </div>
+            
           </div>
+            <div className="row">
+            <div className="col">
+            <Button text="Book a session now"/>
+            </div>
+
+            
+            </div>
           </div>
           </div>
 
